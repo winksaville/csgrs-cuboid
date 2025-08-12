6f1ac7acea47a9441fe65ee53e98a75f67fd614d
Wink Saville <wink@saville.com>
2025-08-11 23:53:33 -0700

    feat: Adding gyroid infill
    Not working yet :(

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use clap::{CommandFactory, Parser};
-use csgrs::traits::CSG;
+use csgrs::{float_types::Real, traits::CSG};
 use nalgebra::Vector3;
 
 type Mesh = csgrs::mesh::Mesh<()>;
 
+// TODO: Forbid dimenstions negative dimensions
 #[derive(Parser, Debug)]
 #[command(version, about = "Generate a cuboid mesh and write it to an STL file.", long_about = None)]
 struct Args {
@@ -20,8 +21,22 @@ struct Args {
     height: f64,
 
     /// wall thickness of the panel
-    #[arg(short = 'W', long, default_value = "0")]
+    #[arg(short = 't', long, default_value = "0")]
     wall_thickness: f64,
+
+    /// Gyroid infill resolution
+    #[arg(short = 'r', long, default_value = "0")]
+    resolution: usize,
+
+    /// Gyroid infill period
+    /// This is the distance between the repeating patterns in the gyroid infill.
+    #[arg(short = 'p', long, default_value = "2.0")]
+    period: f64,
+
+    /// Gyroid infill isovalue
+    /// This is the value that determines the density of the gyroid infill.
+    #[arg(short = 'i', long, default_value = "0.0")]
+    iso_value: f64,
 }
 
 fn main() {
@@ -43,15 +58,17 @@ fn main() {
         outside_dimensions.z,
         None,
     );
+
+    // Create a hollow cuboid mesh by subtracting the inside from the outside
+    let inside_dimensions = outside_dimensions
+        - Vector3::new(
+            args.wall_thickness * 2.0,
+            args.wall_thickness * 2.0,
+            args.wall_thickness * 2.0,
+        );
+    println!("inside_dimenstions: {:?}", inside_dimensions);
+
     if args.wall_thickness > 0.0 {
-        // Create a hollow cuboid mesh by subtracting the inside from the outside
-        let inside_dimensions = outside_dimensions
-            - Vector3::new(
-                args.wall_thickness * 2.0,
-                args.wall_thickness * 2.0,
-                args.wall_thickness * 2.0,
-            );
-        println!("inside_dimenstions: {:?}", inside_dimensions);
         let inside: Mesh = Mesh::cuboid(
             inside_dimensions.x,
             inside_dimensions.y,
@@ -65,15 +82,53 @@ fn main() {
         );
 
         outside = outside.difference(&inside);
-        let center_dimensions = (outside_dimensions + inside_dimensions) / 2.0;
-        println!("center_dimensions: {:?}", center_dimensions);
     }
+
+    if args.resolution > 0 {
+        println!(
+            "Generating gyroid infill with resolution: {}, period: {:.2}, iso_value: {:.2}",
+            args.resolution, args.period, args.iso_value
+        );
+
+        let center_dimensions = (outside_dimensions + inside_dimensions) / 2.0;
+        //let center_dimensions = inside_dimensions;
+        println!("center_dimensions: {:?}", center_dimensions);
+
+        // Generate an infill pattern with the ceter_dimensions size
+        let mut infill_cuboid = Mesh::cuboid(
+            center_dimensions.x,
+            center_dimensions.y,
+            center_dimensions.z,
+            None,
+        )
+        .translate(
+            args.wall_thickness,
+            args.wall_thickness,
+            args.wall_thickness,
+        );
+        let resolution: usize = args.resolution; // resolution of the gyroid infill
+        let period: Real = args.period as Real; // period of the gyroid infill
+        let iso_value: Real = args.iso_value as Real; // isovalue of the gyroid infill
+        infill_cuboid = infill_cuboid.gyroid(resolution, period, iso_value, None);
+    
+        outside = outside.union(&infill_cuboid);
+    }
+
+    let infill_names = if args.resolution > 0 {
+            format!("_r-{:.2}", args.resolution) 
+            + &format!("_p-{:.2}", args.period)
+            + &format!("_i-{:.2}", args.iso_value)
+        } else {
+            String::new()
+        };
 
     // Write the result as an ASCII STL:
     let name = "cuboid".to_owned()
         + &format!("_w-{:.2}", args.width)
         + &format!("_l-{:.2}", args.length)
-        + &format!("_h-{:.2}", args.height);
+        + &format!("_h-{:.2}", args.height)
+        + &format!("_t-{:.2}", args.wall_thickness)
+        + &infill_names; 
     let stl = outside.to_stl_ascii(&name);
     std::fs::write(name.to_owned() + ".stl", stl).unwrap();
 }
